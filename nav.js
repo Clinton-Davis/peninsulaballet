ed56541f40d0fa469b7114df5072851dc432cbe1
Clinton-Davis <clintongadavis@gmail.com>
2020-03-21 08:16:29 +0000

    added grid to secdule and image

--- nav.js
+++ nav.js
@@ -13,14 +13,11 @@ const navSlide = () => {
         }else {
             link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
         }
-
          });
-
          //burger animation
          burger.classList.toggle('toggle');
 
     });
-
 }
 
 navSlide(); 
