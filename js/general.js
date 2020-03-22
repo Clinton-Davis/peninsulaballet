00358a9864dbf332052b4e5e1d58cf7a86fa3eb2
Clinton-Davis <clintongadavis@gmail.com>
2020-03-22 23:21:04 +0000

    tryed something new

---
+++ js/general.js
@@ -0,0 +1,17 @@
+$(function() {
+    $('#testiLink').click(function () { 
+       $('#navHeading').fadeOut(500, function() {
+        $("#navHeading").html('<h4>Working</h4>');
+       });
+       $("#navHeading").fadeIn(500);
+       
+       
+       
+      
+    });
+
+
+
+
+
+})
