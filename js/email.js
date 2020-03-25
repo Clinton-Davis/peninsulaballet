00c31489e05a1bed23ab1060ed35a15614e09c89
Clinton-Davis <clintongadavis@gmail.com>
2020-03-25 10:22:17 +0000

    added email js ans is working

--- js/email.js
+++ js/email.js
@@ -1,19 +1,19 @@
-function sendMail(contactForm) {
-	emailjs
-		.send("gmail", "peninsulaballet", {
-			from_name: contactForm.name.value,
-			from_email: contactForm.emailaddress.value,
-			last_name: contactForm.name.value
-		})
-		.then(
-			function(responce) {
-				if (responce.status == 200) {
-					location.href = "index.html";
-				}
-			},
-			function(error) {
-				console.log("FAILED", error);
-			}
-		);
-	return false;
-}
+var enrolform = $("form#enrolform");
+enrolform.submit(function(event) {
+  event.preventDefault();
+
+
+  var service_id = "default_service";
+  var template_id = "peninsulaballet";
+
+  enrolform.find("button").text("Sending...");
+  emailjs.sendForm(service_id, template_id, enrolform[0])
+    .then(function() {
+      alert("Thank You, you enrol form has been Sent. We will get back to you soon.");
+      enrolform.find("button").text("Send");
+    }, function(err) {
+      alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+      enrolform.find("button").text("Send");
+    });
+  return false;
+});
