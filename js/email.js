f70bf7de86e6be0b7967d27eedd6cd6a3fbb952c
Clinton-Davis <clintongadavis@gmail.com>
2020-03-25 13:28:38 +0000

    added the right code and have form working

--- js/email.js
+++ js/email.js
@@ -2,15 +2,22 @@
 function sendMail(contactForm) {
 	emailjs
 		.send("southpeninsulaballet", "peninsulaballet", {
-			from_name: contactForm.name.value,
-			from_email: contactForm.emailaddress.value,
-			message_request: contactForm.message.value
+			first_name: contactForm.first_name.value,
+			last_name: contactForm.last_name.value,
+			from_email: contactForm.from_email.value,
+			dateOfbirth: contactForm.dateOfbirth.value,
+   			age: contactForm.age.value,
+   			guardian_name: contactForm.guardian_name.value,
+   			contact_number: contactForm.contact_number.value,
+   			emergency_number: contactForm.emergency_number.value,
+			issues: contactForm.issues.value
 		})
+
 		.then(
 			function(responce) {
 				console.log("SUCCESS", responce);
 				if (responce.status == 200) {
-					
+					/*location.href = "index.html";*/
 				}
 			},
 			function(error) {
