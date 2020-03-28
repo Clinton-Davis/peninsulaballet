8f2bdb879183dae6d5812b99d590e357ed5444d2
Clinton-Davis <clintongadavis@gmail.com>
2020-03-28 08:41:03 +0000

    added padding to paragraphs and changed speed of wSroll

--- js/paralax.js
+++ js/paralax.js
@@ -5,7 +5,7 @@ $(window).scroll(function() {
 function parallax() {
     var wScroll = $(window).scrollTop();
     
-    $('.parallax-bg').css('background-position','center ' +(wScroll*0.5)+'px')
+    $('.parallax-bg').css('background-position','center ' +(wScroll*0.6)+'px')
 
     $('.parallax-box').css('top', -2+(wScroll*0.0180)+'em')
 
