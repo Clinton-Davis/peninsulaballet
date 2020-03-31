1a9d7bb8c009cc54a0391c26005e64903f792945
Clinton-Davis <clintongadavis@gmail.com>
2020-03-31 22:43:03 +0100

    added zindex to modal to make sure it stayes ontop

---
+++ js/modal.js
@@ -0,0 +1,64 @@
+function tracyBio() {
+    var modal = document.getElementById("tracyModal");
+    // Get the button that opens the modal
+    var btn = document.getElementById("tracyBtn");
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+    // When the user clicks on the button, open the modal
+    btn.onclick = function () {
+        modal.style.display = "block";
+    }
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
+
+function leilaniBio() {
+    var modal = document.getElementById("leilaniModal");
+    // Get the button that opens the modal
+    var btn = document.getElementById("leilaniBtn");
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close2")[0];
+    // When the user clicks on the button, open the modal
+    btn.onclick = function () {
+        modal.style.display = "block";
+    }
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
+function hitoryBio() {
+    var modal = document.getElementById("historyModal");
+    // Get the button that opens the modal
+    var btn = document.getElementById("historyBtn");
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close3")[0];
+    // When the user clicks on the button, open the modal
+    btn.onclick = function () {
+        modal.style.display = "block";
+    }
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
