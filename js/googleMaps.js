b0013b86bd54e655fda1d69c79c017e8900bd7e6
Clinton-Davis <clintongadavis@gmail.com>
2020-04-01 23:52:49 +0100

    added lng lat to maps

--- js/googleMaps.js
+++ js/googleMaps.js
@@ -2,11 +2,11 @@ console.log("Hello World");
 
 function initMap() {
     // The location of Uluru
-    var uluru = {lat: -25.344, lng: 131.036};
+    var capeTown = {lat: -34.13625 , lng: 18.43042};
     // The map, centered at Uluru
     var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 4, center: uluru});
+        document.getElementById('map'), {zoom: 11, center: capeTown});
     // The marker, positioned at Uluru
-    var marker = new google.maps.Marker({position: uluru, map: map});
+    var marker = new google.maps.Marker({position: capeTown, map: map});
   }
   
