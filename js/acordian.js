68cbc557b1d9c0831ff17b0eb102f5fefde5a9b0
Clinton-Davis <clintongadavis@gmail.com>
2020-04-07 22:41:13 +0100

    added drops downs to studios and changed maps to studios

---
+++ js/acordian.js
@@ -0,0 +1,28 @@
+$( function() {
+    $( "#fishHoek" ).accordion({
+      active: false,
+      collapsible: true
+    });
+
+    $( "#muizenBerg" ).accordion({
+        active: false,
+        collapsible: true
+      });
+
+    $("#kommetJie").accordion({
+        active: false,
+        collapsible: true
+    });
+
+    $("#kirstenHof").accordion({
+        active: false,
+        collapsible: true
+    });
+    
+    $("#outLets").accordion({
+        active: false,
+        collapsible: true
+    });
+
+
+})
