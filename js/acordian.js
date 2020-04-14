d4f22db1ca47b7309c577ed15dfc2112c718d22c
Clinton-Davis <clintongadavis@gmail.com>
2020-04-14 23:50:45 +0100

    centered studios on page put outlets on dottom of page

--- js/acordian.js
+++ js/acordian.js
@@ -1,32 +1,8 @@
 $( function() {
 
-    
     /** Accordion fuction make the studio loacation drop down and up */
-    
-    $( "#fishHoek" ).accordion({
-      active: false,
-      collapsible: true
-    });
-
-    $( "#muizenBerg" ).accordion({
-        active: false,
-        collapsible: true
-      });
-
-    $("#kommetJie").accordion({
-        active: false,
-        collapsible: true
-    });
-
-    $("#kirstenHof").accordion({
-        active: false,
-        collapsible: true
-    });
-    
     $("#outLets").accordion({
         active: false,
         collapsible: true
     });
-
-
 })
