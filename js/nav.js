f879b2baf984f0f8f9eae3fd05e37d29a3cf0ed9
Clinton Davis <53115265+Clinton-Davis@users.noreply.github.com>
2020-11-28 21:16:05 +0000

    Restructured the javascript to work more efficantly

--- js/nav.js
+++ js/nav.js
@@ -2,9 +2,16 @@ const burger = document.querySelector(".burger");
 const navLinks = document.querySelector(".nav-links");
 const navLinks_Li = document.querySelectorAll(".nav-links li");
 //* Opens & Closed burger menu on buger click
-const navSlide = () => {
-  //Toggle Nav
-  burger.addEventListener("click", () => {
+
+const getBuger = () => {
+  burger.addEventListener("click", navSlide(burger));
+};
+const getLink = () => {
+  navLinks.addEventListener("click", navSlide(navLinks));
+};
+function navSlide(thing) {
+  thing.addEventListener("click", () => {
+    //Toggle Nav
     navLinks.classList.toggle("nav-active");
 
     //Animate Links
@@ -20,26 +27,7 @@ const navSlide = () => {
     //burger animation
     burger.classList.toggle("toggle");
   });
-};
-//* closes nav and buger on navLint_li selection
-const backSlide = () => {
-  //Toggle Nav
-  navLinks.addEventListener("click", () => {
-    navLinks.classList.toggle("nav-active");
+}
 
-    //Animate Links
-    navLinks_Li.forEach((link, index) => {
-      if (link.style.animation) {
-        link.style.animation = "";
-      } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${
-          index / 7 + 0.5
-        }s`;
-      }
-    });
-    //burger animation
-    burger.classList.toggle("toggle");
-  });
-};
-navSlide();
-backSlide();
+getBuger();
+getLink();
