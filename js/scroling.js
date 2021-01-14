f87ba8f801977356835b0b1b191683834e609cc4
Clinton Davis <53115265+Clinton-Davis@users.noreply.github.com>
2021-01-14 11:00:50 +0000

    changedthe loctations of the images in cloudanry and in events,fixed somehtml syntex errors

--- js/scroling.js
+++ js/scroling.js
@@ -12,17 +12,6 @@ $(function () {
     );
   });
 
-  $("html").on("click", ".ripple-surface", function (evt) {
-    let btn = $(evt.currentTarget);
-    let x = evt.pageX - btn.offset().left;
-    let y = evt.pageY - btn.offset().top;
-
-    $("<span/>").appendTo(btn).css({
-      left: x,
-      top: y,
-    });
-  });
-
   $(".btt-link").click(function (e) {
     $("html").animate(
       {
